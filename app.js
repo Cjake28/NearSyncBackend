5b5aa1fe6f11eaa3b7001ccb315a137dee23e809
Cjake28 <cjsupnet28@gmail.com>
2025-02-23 21:57:44 +0800

    create error handling

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import {errorHandler} from './middleware/errorHandler.js';
+
+dotenv.config();
+
+export const app = express();
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://attendeasyai.up.railway.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); 
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(errorHandler);
